a501ddec6389d81fe9886e6f60141a7e5d6490ac
Carolina <carolina.reis@nava.com.br>
2022-09-28 16:11:32 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/caroly/projeto_oficial/ProjetoOficialApplication.java
@@ -0,0 +1,13 @@
+package br.com.caroly.projeto_oficial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOficialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoOficialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caroly/projeto_oficial/ProjetoOficialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caroly.projeto_oficial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoOficialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
