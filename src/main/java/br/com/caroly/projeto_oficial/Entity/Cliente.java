8e807d392662cc76a8f308284850ba6b5f16015e
Carolina <carolina.reis@nava.com.br>
2022-09-28 16:11:07 -0300

    Criação da entidade cliente

---
+++ src/main/java/br/com/caroly/projeto_oficial/Entity/Cliente.java
@@ -0,0 +1,31 @@
+package br.com.caroly.projeto_oficial.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+//Estou usando o lombok para Getter e Setters
+
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String sobrenome;
+    @Column(nullable = false)
+    private String email;
+    private int idade;
+    private String telefone;
+    
+}
