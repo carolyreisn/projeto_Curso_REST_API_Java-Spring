4710d90b4f787921959f8098d9ea4b7d3197b2a4
Carolina <carolina.reis@nava.com.br>
2022-09-28 16:20:35 -0300

    Adicionando argumento faltante

--- src/main/java/br/com/caroly/projeto_oficial/Entity/Cliente.java
+++ src/main/java/br/com/caroly/projeto_oficial/Entity/Cliente.java
@@ -7,9 +7,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 //Estou usando o lombok para Getter e Setters
